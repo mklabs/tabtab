6c1b1bb49cfc9ad641e350a09b3fcb1fb240a20d
Mickael Daniel <daniel.mickael@gmail.com>
2011-10-19 18:59:06 +0200

    completion: add optimist completion, have to parse out the help output
    cause there's no easy way to get optimist options/usage in the current
    API. Note that this will be also the case of cake completion.
    
    basically the parsing tries to match any -short and --long patterns
    that might appear in the output.

--- examples/optimist/usage-options.js
+++ examples/optimist/usage-options.js
@@ -1,19 +1,27 @@
 var optimist = require('optimist');
 
-var argv = optimist.usage('This is my awesome program', {
-  'about': {
-    description: 'Provide some details about the author of this program',
-    required: true,
-    short: 'a',
-  },
-  'info': {
-    description: 'Provide some information about the node.js agains!!!!!!',
-    boolean: true,
-    short: 'i'
-  }
-}).argv;
+optimist
+  .usage('This is my awesome program', {
+    'about': {
+      description: 'Provide some details about the author of this program',
+      required: true,
+      alias: 'desc',
+      short: 'a',
+    },
+    'info': {
+      description: 'Provide some information about the node.js agains!!!!!!',
+      boolean: true,
+      short: 'i'
+    }
+  })
+  .alias('b', 'base')
+  .describe('b', 'Numeric base to display the number of lines in')
+  .default('b', 10)
+  .describe('x', 'Super-secret optional parameter which is secret')
+  .default('x', '')
 
-optimist.showHelp();
 
-console.log('\n\nInspecting options');
-console.dir(argv);
+// expose optimist instance so that we can play with it and our completion
+module.exports = optimist;
+
+
