45b09af17c7897573097bb3de774d731c948f1df
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-26 08:32:11 +0200

    Cleanup old dir

--- .old/test/fixtures/completer.js
+++
@@ -1,9 +0,0 @@
-var completion = require('../../lib/completion');
-
-completion.complete('foo', 'foobar', function(err, o) {
-  if(err || !o) return;
-
-  console.log('fooooobar');
-});
-
-if(completion.isComplete()) return;
