82de5ef2cd8bab7c2b5ebe30cce90a786e268ccb
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-26 12:10:45 +0200

    Check in examples

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -1,16 +1,18 @@
-###-begin-{pkgname}-completion-###
-function _{pkgname}_completion
+###-begin-bower-completion-###
+function _bower_completion
   set cmd (commandline -opc)
   set cursor (commandline -C)
-  set completions (eval env DEBUG="tabtab*" COMP_CWORD="$cmd" COMP_LINE="$cmd" COMP_POINT="$cursor" {completer} completion --json -- $cmd)
+  echo $cmd > /tmp/debug.log
+  echo $cursor>> /tmp/debug.log
+  set completions (eval env DEBUG=\""tabtab*\"" COMP_CWORD=\""$cmd\"" COMP_LINE=\""$cmd\"" COMP_POINT=\""$cursor\"" bower-complete completion --json)
 
   for completion in $completions
     set cmd "node -pe \"'$completion'.split(':').join('\n')\""
     set parts (eval $cmd)
-    complete -f -c {pkgname} -a "'$parts[1]'" -d "$parts[2]"
+    complete -f -c bower -a "'$parts[1]'" -d "$parts[2]"
     echo $parts[1]
   end
 end
 
-complete -d 'tabtab' -f -c {pkgname} -a "(eval _{pkgname}_completion)"
-###-end-{pkgname}-completion-###
+complete -d 'tabtab' -c bower -a "(eval _bower_completion)"
+###-end-bower-completion-###
