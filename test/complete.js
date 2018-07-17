6f279be38b197f3f9e9fcf13feceefb48f9638a9
Mickael Daniel <daniel.mickael@gmail.com>
2018-07-17 11:56:37 +0200

    babel: remove babel continuation

--- test/complete.js
+++ test/complete.js
@@ -1,6 +1,6 @@
 const fs       = require('fs');
 const path     = require('path');
-const Complete = require('../src/complete');
+const Complete = require('../lib/complete');
 const assert   = require('assert');
 
 describe('Complete', () => {
