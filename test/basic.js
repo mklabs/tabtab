4c7bce2dc08ffd477d41f8754a1c53a3c7b170c5
Mickael Daniel <daniel.mickael@gmail.com>
2018-07-22 17:51:57 +0200

    test: recover some coverage

--- test/basic.js
+++ test/basic.js
@@ -10,12 +10,46 @@ describe('tabtab basic suite', () => {
   it('has a tabtab.log function', () => {
     assert.equal(typeof tabtab.log, 'function');
 
-    const env = Object.assign({}, process.env, {
+    const environment = Object.assign({}, process.env, {
       COMP_CWORD: 2,
       COMP_LINE: 'tabtab --foo',
       COMP_POINT: 12
     });
 
-    tabtab.log(['--foo', '--bar'], env);
+    const env = tabtab.parseEnv({ _: ['tabtab'] }, environment);
+
+    const result = tabtab.log(['--foo', '--bar'], env);
+    assert.deepEqual(result.args, ['--foo']);
+    assert.deepEqual(result.env, env);
+  });
+
+  it('has a tabtab.log function, without env', () => {
+    const result = tabtab.log(['--foo', '--bar']);
+    assert.deepEqual(result.args, ['--foo', '--bar']);
+    assert.deepEqual(result.env, {
+      args: [],
+      complete: false,
+      last: '',
+      lastPartial: '',
+      line: '',
+      partial: '',
+      point: 0,
+      prev: undefined,
+      words: 0
+    });
+  });
+
+  it('tabtab.shell()', () => {
+    let shell = tabtab.shell();
+    assert.equal(shell, 'bash');
+
+    shell = tabtab.shell({ SHELL: '/bin/bash' });
+    assert.equal(shell, 'bash');
+
+    shell = tabtab.shell({ SHELL: '/usr/bin/zsh'});
+    assert.equal(shell, 'zsh');
+
+    shell = tabtab.shell({ SHELL: '/usr/bin/fish'});
+    assert.equal(shell, 'fish');
   });
 });
