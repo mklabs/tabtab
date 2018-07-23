51f1de7f215dacba5b321daf615ad31d1579eadb
Mickael Daniel <daniel.mickael@gmail.com>
2018-07-23 14:46:35 +0200

    bash: handle semicolon

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -3,7 +3,7 @@ if type complete &>/dev/null; then
   _{pkgname}_completion () {
     local words cword
     if type _get_comp_words_by_ref &>/dev/null; then
-      _get_comp_words_by_ref -n = -n @ -w words -i cword
+      _get_comp_words_by_ref -n = -n @ -n : -w words -i cword
     else
       cword="$COMP_CWORD"
       words=("${COMP_WORDS[@]}")
@@ -16,6 +16,9 @@ if type complete &>/dev/null; then
                            {completer} completion -- "${words[@]}" \
                            2>/dev/null)) || return $?
     IFS="$si"
+    if type __ltrim_colon_completions &>/dev/null; then
+      __ltrim_colon_completions "${words[cword]}"
+    fi
   }
   complete -o default -F _{pkgname}_completion {pkgname}
 fi
